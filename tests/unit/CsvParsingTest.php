d5e834d481f965ae5a04b9ee1d499d2bd4a50f12
Mihail <tsurkanovm@gmail.com>
2016-01-12 13:41:21 +0200

    final version for first iteration (simple example)

--- tests/unit/CsvParsingTest.php
+++ tests/unit/CsvParsingTest.php
@@ -13,10 +13,33 @@ class CsvParsingTest extends \Codeception\TestCase\Test
      */
 
     protected static $data;
+    protected static $parser_handler;
+    protected static $file_path;
+    protected static $options;
 
     public static function setUpBeforeClass()
     {
-        $options =
+
+        self::$file_path = Yii::getAlias('@tests') . '\template.csv';
+        self::$parser_handler = new YiiParserHandler();
+
+    }
+
+    public function parseFileByOptions( $options ){
+
+        self::$parser_handler->setConfiguration( $options );
+        $csv_parser = Stub::make( new YiiMultiparser(), ['parserHandler' => self::$parser_handler] );
+
+        if (!$csv_parser)
+            self::markTestSkipped('Parser handler do not initialized.');
+
+        self::$data = $csv_parser->parse( self::$file_path, ['mode' => 'template'] );
+    }
+
+
+    public function testWithKeys( ){
+
+        self::$options =
             ['csv' =>
                 ['template' =>
                     ['class' => 'yii\multiparser\CsvParser',
@@ -39,47 +62,80 @@ class CsvParsingTest extends \Codeception\TestCase\Test
                 ],
             ];
 
-        $file_path = Yii::getAlias('@tests') . '\template.csv';
-
-        $ph = new YiiParserHandler();
-        $ph->setConfiguration( $options );
-        $csv_parser = Stub::make( new YiiMultiparser(), ['parserHandler' => $ph] );
-
-        if (!$csv_parser)
-            self::markTestSkipped('Parser handler do not initialized.');
-
-        self::$data = $csv_parser->parse( $file_path, ['mode' => 'template'] );
-    }
-
-
-
-    public function testOnEmptyResult( ){
-
+        $this->parseFileByOptions( self::$options );
         $this->assertNotEmpty( self::$data , 'Output array is empty' );
 
     }
 
     /**
-     * @depends testOnEmptyResult
+     * @depends testWithKeys
      */
-    public function testOnAssocResult( ){
+    public function testWithKeysOnFullness( ){
 
         $this->assertArrayHasKey( 'Article', self::$data[0], 'Output array does`t have key - Article'  );
         $this->assertArrayHasKey( 'Count', self::$data[1], 'Output array does`t have key - Count'  );
         $this->assertArrayHasKey( 'Description', self::$data[2], 'Output array does`t have key - Description'  );
         $this->assertArrayHasKey( 'Price', self::$data[3], 'Output array does`t have key - Price'  );
         $this->assertArrayHasKey( 'Price', self::$data[13], 'Output array does`t have key - Brand'  );
+        $this->assertEquals( 16, count( self::$data ), 'Output array does`t have 16 rows'  );
+
+    }
+
+    public function setOptionDataProvider(){
+
+        $opt = [
+            [7, ['csv' => ['template' => ['first_line' => 10],],]],
+            [8, ['csv' => ['template' => ['first_line' => 10,  'has_header_row' => false],],]],
+            [17, ['csv' => ['template' => ['has_header_row' => false],],]],
+            [3, ['csv' => ['template' => ['last_line' => 3, 'has_header_row' => false],],]],
+            [2, ['csv' => ['template' => ['last_line' => 3],],]]
+        ];
+
+        return $opt;
+    }
+
+    /**
+     * @depends testWithKeys
+     * @dataProvider setOptionDataProvider
+     */
+    public function testWithKeysOnTruncate( $expectedResult,  $sub_options ){
+
+        $options = array_merge_recursive( self::$options, $sub_options );
+        $this->parseFileByOptions( $options );
+        $this->assertEquals( $expectedResult, count( self::$data ), "Output (extended) array does`t have {$expectedResult} rows"  );
 
     }
 
     /**
-     * @depends testOnEmptyResult
+     * @depends testWithKeys
      */
-    public function testOnFullResult( ){
+    public function testWithoutKeys( ){
 
-        $this->assertEquals( 16, count( self::$data ), 'Output array does`t have 16 rows'  );
+        self::$options =
+            ['csv' =>
+                ['template' =>
+                    ['class' => 'yii\multiparser\CsvParser',
+
+                    ],
+                ],
+            ];
+
+        $this->parseFileByOptions(  self::$options );
+        $this->assertNotEmpty( self::$data , 'Output array is empty' );
 
     }
+
+    /**
+     * @depends testWithoutKeys
+     */
+    public function testWithoutKeysOnFullness( ){
+
+        $this->assertEquals( 16, count( self::$data ), 'Output array does`t have 16 rows'  );
+        $this->assertEquals( 5, count( self::$data[0] ), 'Output array does`t have 5 columns'  );
+
+    }
+
+
 
 
 
